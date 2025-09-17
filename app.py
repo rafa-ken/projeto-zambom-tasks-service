516f2485a8d20c63e60884153dbae0ca58d99ed6
rafa-ken <rafaelkenmiyamoto@gmail.com>
2025-09-17 14:55:56 -0300

    voltando a porta 5000

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def deletar_tarefa(id):
 # --------------------------------------- #
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
